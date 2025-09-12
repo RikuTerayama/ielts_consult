76acaba944faca1d086bb7d11f9b227ab7eb4626
RikuTerayama <rikuriku619center@gmail.com>
2025-09-13 00:30:05 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL || 'https://<your-site>.netlify.app', // ← 後で置換
+  site: process.env.PUBLIC_SITE_URL || 'https://<your-site>.netlify.app', // TODO: 一時URLでOK
   integrations: [sitemap()],
   output: 'static'
 });
